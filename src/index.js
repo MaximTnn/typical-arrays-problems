cb76365e61490119b84b7fc2888b719054d765d7
Max <tyumenbaev.m@gmail.com>
2021-03-13 10:26:50 +0300

    feat: add solution in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = [0]) {
+     return array.length == 0 ? 0 : Math.min.apply(Math, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = [0]) {
+    return array.length == 0 ? 0 : Math.max.apply(Math, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = [0]) {
+    let sum = 0
+    array.forEach(element => {sum += element});
+    return  array.length == 0 ? 0 : (sum / array.length);
 }
+
+
